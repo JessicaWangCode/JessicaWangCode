4c4aaad69edffd7da7cc5ea9965abb0c92f83e99
Yiyao Wang <78681139+JessicaWangCode@users.noreply.github.com>
2022-05-06 15:21:22 +0100

    time-series machine learning

--- MachineLearningPred.py
+++ MachineLearningPred.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Apr 18 16:48:50 2022
-
-@author: 27666
-"""
-# -*- coding: utf-8 -*-
-"""
 Created on Fri Apr  1 23:25:44 2022
 
 @author: 27666
@@ -20,22 +14,36 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import MinMaxScaler
 from sklearn import metrics
 import seaborn as sns
-
+import matplotlib.pyplot as plt
 
 #ASK FOR INDEX
 def index():
-    index = input("Choose for Covid index:")
+    answer = input("Choose for Covid index:\nCumulative Cases(1) \nCumulative Death(2) \nDaily New Case(3) \n")
+    while True:
+        if answer == '1':
+            index = "cumCasesBySpecimenDate"
+            break
+        elif answer == '2':
+            index = 'cumDeaths28DaysByPublishDate'
+            break
+        elif answer == '3':
+            index = 'newCasesBySpecimenDate'
+            break
+        else:
+            print("error. Please enter again.")
+            answer = input("Choose for Covid index:\nCumulative Cases(1) \nCumulative Death(2) \nDaily New Case(3) \n")
+        
     return(index)
 
 def create_assist_date(datestart = None,dateend = None):
-	# Create out of sample datelist
+	# CREATE OUT OF SAMPLE DATELIST
 
 	if datestart is None:
 		datestart = '01/01/2022'
 	if dateend is None:
 		dateend = datetime.datetime.now().strftime("%d/%m/%Y")
 
-	# Transfer to date
+	# TRANSFER TO DATE
 	datestart=datetime.datetime.strptime(datestart,"%d/%m/%Y")
 	dateend=datetime.datetime.strptime(dateend,"%d/%m/%Y")
 	date_list = []
@@ -51,9 +59,9 @@ def create_assist_date(datestart = None,dateend = None):
 	return(date_list)
 
 
-#Data Cleaning
+#DATA CLEANING
 def DataPreparation(COVID_df,index,datelist):
-    #DATA CLEANING
+    
     COVID_df['date'] = COVID_df['date'].apply(lambda x: datetime.datetime.strptime(x, "%d/%m/%Y"))
     COVID_df['date'] = pd.to_numeric(COVID_df.date)
     
@@ -61,8 +69,7 @@ def DataPreparation(COVID_df,index,datelist):
     datelist.columns = ['ds']
     datelist['ds'] = datelist['ds'].apply(lambda x: datetime.datetime.strptime(x, "%d/%m/%Y"))
     datelist['ds'] = pd.to_numeric(datelist.ds)
-    np.where(COVID_df[index].values >= np.finfo(np.float64).max)
-    np.where(COVID_df['date'].values >= np.finfo(np.float64).max)
+    
     
     COVID_df_cleaned = COVID_df.dropna(axis=0,subset = [index]) #drop all rows that have any NaN values
    
@@ -73,7 +80,7 @@ def DataPreparation(COVID_df,index,datelist):
     # SPLITING DATASET INTO TRAINING SET AND TEST SET. 
     X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=0)
     
-    #Standardization
+    #STANDARDIZATION
     scalerX1 = MinMaxScaler()
     scalerY1 = MinMaxScaler()
     x_train1 = scalerX1.fit_transform(np.array(X_train).reshape(-1,1))
@@ -92,26 +99,28 @@ class COVID_ML():
         self.datelist = datelist
         self.x_test1 = x_test1
     def SVM_IS_Predict(self):  
-        #SVR Model with in-sample prediction
+        #SUPPORT VECTOR MACHINE MODEL WITH IN-SAMPLE PREDICTION
         svm=SVR(kernel="rbf", C=1,epsilon=0.01,gamma='scale')
         svm.fit(self.x_train1,self.y_train1)
         y_pred_IS=self.scalerY1.inverse_transform(np.array(svm.predict(self.x_test1)).reshape(-1,1))
         y_pred_IS=np.array(y_pred_IS).reshape(-1)
         return y_pred_IS
     def LR_IS_Predict(self):
+        #LINEAR REGRESSION MODEL WITH IN-SAMPLE PREDICTION
         regressor = LinearRegression()
         regressor.fit(self.x_train1, self.y_train)
         y_pred_IS = regressor.predict(self.x_test1)
         y_pred_IS=np.array(y_pred_IS).reshape(-1)
         return y_pred_IS
     def SVM_OS_Predict(self):  
-        #SVM Model with out-sample prediction
+        #SUPPORT VECTOR MACHINE MODEL WITH OUT-SAMPLE PREDICTION
         svm=SVR(kernel="rbf", C=1,epsilon=0.01,gamma='scale')
         svm.fit(self.x_train1,self.y_train1)
         y_pred_OS=self.scalerY1.inverse_transform(np.array(svm.predict(self.datelist)).reshape(-1,1))
         y_pred_OS=np.array(y_pred_OS).reshape(-1)
         return y_pred_OS
     def LR_OS_Predict(self):
+        #LINEAR REGRESSION MODEL WITH IN-SAMPLE PREDICTION
         regressor = LinearRegression()
         regressor.fit(self.x_train1, self.y_train)
         y_pred_OS = regressor.predict(self.datelist)
@@ -120,31 +129,36 @@ class COVID_ML():
     
     
 def comparison(y_test,X_test,y_pred_IS):
+        #EVALUATE LEARNING RESULT
         dff = pd.DataFrame({'Current': y_test, 'Predicted': y_pred_IS})
         print(dff.head())
         print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred_IS))  
         X_test = pd.to_datetime(X_test)
         sns.scatterplot(X_test,y_test,color='#A7A7A7',s=75,label="Actual")
         sns.lineplot(X_test,np.array(y_pred_IS).reshape(-1),color="#A4D54E",linewidth=2.25,label="Predicted")
-
+        plt.show()
 def main():
-    #LOAD DATA
+    #LOAD TRAINING DATA
     COVID_df = pd.read_csv("overview_2022-04-01 (1).csv")
-    
+    #CREATE DATELIST
     datelist = create_assist_date('30/01/2020')
-
-    x_train1,y_train1,y_test,X_test,x_test1,y_train,scalerY1,datelist = DataPreparation(COVID_df,index(),datelist)
-
-    c = COVID_ML(x_train1,y_train1,y_train,scalerY1,datelist,x_test1)
-    method = c.SVM_OS_Predict()
+    
+    x_train1,y_train1,y_test,X_test,x_test1,y_train,scalerY1,datelist = DataPreparation(COVID_df,'newCasesBySpecimenDate',datelist)
+    
+    ML = COVID_ML(x_train1,y_train1,y_train,scalerY1,datelist,x_test1)
+    
+    #CHOOSE A MACHINELEARNING MODEL
+    method = ML.SVM_OS_Predict()
+    #GET MACHINE LEARNING RESULT
     predict = np.array(method.reshape(-1))
-    if method.shape == c.SVM_IS_Predict().shape or method.shape ==c.LR_IS_Predict().shape:
+    
+    #ENSURE ONLY IN SAMPLE PREDICTION WILL TRIGGER COMPARISON
+    if method.shape == ML.SVM_IS_Predict().shape or method.shape ==ML.LR_IS_Predict().shape:
         comparison(y_test,X_test,predict)
     return predict
     
 if __name__ == '__main__':
     main()    
-
 
 
 
--- compare.py
+++ compare.py
@@ -9,28 +9,54 @@ import numpy as np
 import pandas as pd
 import MachineLearningPred as mlp
 from sklearn import metrics
+import SIRmodel as SIR
+
 def main():
     ML_Pred= mlp.main()
-    Realdata_df = pd.read_csv("test.csv")[::-1] 
-
-
-    fig = plt.figure()
-    ax = fig.add_axes([0,0,1,1])
+    Realdata_df = pd.read_csv("real_data.csv")[::-1] 
+    df = SIR.getDataFrame(53500000, 0.13, 0.114)
+    
+    #SET MACHINE LEARNING DATA
     y1=ML_Pred
     x1=np.arange(len(y1))
-    print(x1.shape) #TEST LINE
-    y2=Realdata_df['number']
-    print(y2.shape) #TEST LINE
-    plt.bar(x1,y1,label="machine learning",color='b')
+    
+    #print(x1.shape) #TEST LINE
+    
+    #SET REAL DATA
+    y2=Realdata_df['newcase']
     x2 = np.arange(len(y2))
-    plt.bar(x2,y2,color='r',label="real data")
+    #print(y2.shape) #TEST LINE    
+    
+    #SET SIMULATION DATA
+    y3=df['infected']
+    x3 = np.arange(len(y3))
+    
+    #EVALUATE MODEL BY COMPARING WITH REAL DATA QUANTITATIVELY
+    EV_ML=metrics.mean_squared_error(Realdata_df['newcase'], ML_Pred[0:len(y2)])
+    EV_TH = metrics.mean_squared_error(Realdata_df['newcase'], df['infected'][0:len(y2)])
+    
+    #Plot real data
+    plt.bar(x2,y2,color='r',label='real data')
+    #Plot machine learning result
+    plt.bar(x1,y1,label='machine learning result:%s'%(EV_ML),color='b')
+    #Plot SIR model result
+    plt.bar(x3,y3,color='g',label='SIR model result:%s'%(EV_TH))
+    
+    #STYLE&LEGEND
     plt.legend()
-    plt.xlabel("Days")
+    plt.grid(b=True, which='major', axis='y', color='#888888', linestyle='--')
+    plt.xlabel("Days Since Pandemic Outbreak")
     plt.ylabel("Number of People")
-    plt.title("Comparison")
+    plt.title("Comparison of Daily New Cases")
     plt.show()
-
-    print('Mean Squared Error:', metrics.mean_squared_error(Realdata_df['number'], ML_Pred[0:len(y2)])) 
     
 if __name__ == '__main__':
     main()
+    
+    
+    
+    
+    
+    
+    
+    
